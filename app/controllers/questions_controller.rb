bc4a37be020c95f1f35b4e60d8b923f3c1c25ef2
Arthur Picard <arthur.picard7@gmail.com>
2021-11-08 14:32:53 +0100

    Corrected offenses

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,11 +4,11 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    @answer = ""
-    if @question == "What time is it?"
-      @answer = "Silly question, get dressed and go to work!"
-    elsif @question == "I am going to work"
-      @answer = "Great!"
+    @answer = ''
+    if @question == 'What time is it?'
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question == 'I am going to work'
+      @answer = 'Great!'
     else
       @answer = "I don't care, get dressed and go to work!"
     end
